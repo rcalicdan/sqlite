ff375318f7dd608fd1d0e42041403b94a950eb45
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2026-06-20 13:34:12 +0800

    refactor: remove unused CPU count and environment validation methods from SystemHelper

--- src/Utilities/SystemHelper.php
+++ src/Utilities/SystemHelper.php
@@ -18,11 +18,6 @@ final class SystemHelper
     private static ?string $autoloadPath = null;
 
     /**
-     * @var int<1, max>|null
-     */
-    private static ?int $cpuCount = null;
-
-    /**
      * Disable instantiation.
      */
     private function __construct()
@@ -106,92 +101,6 @@ final class SystemHelper
         }
 
         throw new \RuntimeException('Failed to locate vendor/autoload.php. Make sure Composer has been run.');
-    }
-
-    /**
-     * Determines the number of logical CPU cores on the host machine.
-     * Highly useful for dynamically sizing async connection pools.
-     *
-     * @return int<1, max> Minimum of 1 logical core
-     */
-    public static function getCpuCount(): int
-    {
-        if (self::$cpuCount !== null) {
-            return self::$cpuCount;
-        }
-
-        $count = 4;
-
-        if (\function_exists('shell_exec')) {
-            $command = match (PHP_OS_FAMILY) {
-                'Windows' => 'wmic cpu get NumberOfLogicalProcessors /value',
-                'Darwin' => 'sysctl -n hw.logicalcpu',
-                default => 'nproc',
-            };
-
-            $output = @\shell_exec($command);
-
-            if (\is_string($output) && \trim($output) !== '') {
-                if (PHP_OS_FAMILY === 'Windows' && \preg_match('/NumberOfLogicalProcessors=(\d+)/', $output, $m) === 1) {
-                    $count = (int) $m[1];
-                } elseif (($val = (int) \trim($output)) > 0) {
-                    $count = $val;
-                }
-            }
-        }
-
-        if ($count === 4 && PHP_OS_FAMILY === 'Linux') {
-            if (\is_readable('/sys/devices/system/cpu/present')) {
-                $content = \trim((string) \file_get_contents('/sys/devices/system/cpu/present'));
-                if (\preg_match('/^(\d+)-(\d+)$/', $content, $m) === 1) {
-                    $count = (int) $m[2] - (int) $m[1] + 1;
-                }
-            } elseif (\is_readable('/proc/cpuinfo')) {
-                $cpuInfo = (string) \file_get_contents('/proc/cpuinfo');
-                $matchCount = \preg_match_all('/^processor/m', $cpuInfo, $cpuMatches);
-                if ($matchCount !== false && $matchCount > 0) {
-                    $count = $matchCount;
-                }
-            }
-        }
-
-        if ($count === 4 && PHP_OS_FAMILY === 'Windows') {
-            $env = \getenv('NUMBER_OF_PROCESSORS');
-            if ($env !== false && (int) $env > 0) {
-                $count = (int) $env;
-            }
-        }
-
-        /** @var int<1, max> $finalCount */
-        $finalCount = \max(1, $count);
-        self::$cpuCount = $finalCount;
-
-        return $finalCount;
-    }
-
-    /**
-     * Ensures that necessary PHP functions are enabled in the environment.
-     * Throws a ConnectionException if required process management features are disabled.
-     *
-     * @throws ConnectionException
-     */
-    public static function validateEnvironment(): void
-    {
-        $requiredFunctions = ['proc_open', 'exec', 'shell_exec'];
-
-        $missingFunctions = \array_filter($requiredFunctions, static function (string $function): bool {
-            return ! \function_exists($function);
-        });
-
-        if (\count($missingFunctions) > 0) {
-            throw new ConnectionException(
-                \sprintf(
-                    'The following required PHP functions are disabled in your php.ini: "%s". ' .
-                        'Hibla SQLite requires these functions to manage background worker daemons.',
-                    \implode('", "', $missingFunctions)
-                )
-            );
-        }
     }
 
     /**
